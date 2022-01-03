78a3fb2cef3a2fcfab917b0e943594a26ce5429c
Aishaml1 <Aisha.lugg1@icloud.com>
2022-01-02 22:07:06 -0500

    color change

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
+
+
 
 
 const Contact = () => {
@@ -6,6 +9,11 @@ const Contact = () => {
 return (
     <div className="about">
     <h1>Contact me </h1>
+    <h1> 
+    <AiFillGithub />
+    <AiFillLinkedin />
+    <AiFillTwitterCircle />
+</h1>
     </div>
 )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import '../styles/home.css';
-import flower from '../assets/Images/flowerr.png'
+import flower from '../assets/Images/A-logo.png'
+import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
 
 const Home = () => {
 
     return (
-        <div className="home">
-            <h1 className='intro' > Hi there, my name is Aisha. </h1>
+        <div className="home">   
+ {/* <h1 className='links'> 
+    <AiFillGithub />
+    <AiFillLinkedin />
+    <AiFillTwitterCircle />
+</h1>     */}
+<h1 className='intro' > Hi there, my name is Aisha. </h1>
+<h1 className='intro2' >- <i>Software Engineer</i> </h1>
+
 <img className='homelogo' src={flower} alt="Aisha logo" />
+
         </div>
     )
 }
