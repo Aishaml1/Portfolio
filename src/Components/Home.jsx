83d575953e94adcbd5510a582bf42c3753f7368b
Aishaml1 <Aisha.lugg1@icloud.com>
2022-01-02 22:11:38 -0500

    color changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import '../styles/home.css';
 import flower from '../assets/Images/A-logo.png'
-import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
 
 const Home = () => {
 
     return (
         <div className="home">   
- {/* <h1 className='links'> 
-    <AiFillGithub />
-    <AiFillLinkedin />
-    <AiFillTwitterCircle />
-</h1>     */}
+
 <h1 className='intro' > Hi there, my name is Aisha. </h1>
 <h1 className='intro2' >- <i>Software Engineer</i> </h1>
 
