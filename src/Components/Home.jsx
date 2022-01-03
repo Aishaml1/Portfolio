3cea55a13da77dc02fbbcf39ee107dc3c48173d3
Aishaml1 <Aisha.lugg1@icloud.com>
2022-01-03 18:39:29 -0500

    resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 <h1 className='contact'> 
     <a  className='contact1' href='https://github.com/Aishaml1'> <AiFillGithub /> </a>
     <a  className='contact2' href='https://www.linkedin.com/in/aisha-lugg/'><AiFillLinkedin /> </a>
-    <a className='Resume' href='https://docs.google.com/document/d/1UHoaFswxDLOzQvpnqcw-eH97zv2zTEsF/edit?usp=sharing&ouid=101812758702735989897&rtpof=true&sd=true'> Resume  </a>
+    <a className='Resume' href='https://docs.google.com/document/d/e/2PACX-1vRux-6TrkUL_yP0EIwkAB_TX4vbLcQkT79rTVkVksIkFQQLFDX3jozu1gMhpYi1N7VVpEzxQAgZCSMT/pub'> Resume  </a>
 </h1>
 <img className='homelogo' src={flower} alt="Aisha logo" />
 
